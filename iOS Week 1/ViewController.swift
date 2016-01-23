1b0e313e4a5119a7b8fbb0683d38fabdee4548ce
Steven Povlitz <steven.povlitz@gmail.com>
2016-01-23 18:00:31 -0500

    Initial Commit

---
+++ iOS Week 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Week 1
+//
+//  Created by Steven Povlitz on 1/23/16.
+//  Copyright © 2016 Steven Povlitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
